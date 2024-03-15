c41e190797a090c9adcf8c70fe380eac1b40e98b
yc896 <yc896@drexel.edu>
2024-03-15 10:46:23 -0400

    First commit

---
+++ build/precache-manifest.0e3d5d66acc8243c1a1193b86c311d28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8b3b2df8947d48b4a0290a0084ea79",
+    "url": "/INFO655/react-playlist/index.html"
+  },
+  {
+    "revision": "1beb52bfedd10a66c36b",
+    "url": "/INFO655/react-playlist/static/css/main.8f530957.chunk.css"
+  },
+  {
+    "revision": "8822e8a4086e073aa836",
+    "url": "/INFO655/react-playlist/static/js/2.7bd8efa2.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/INFO655/react-playlist/static/js/2.7bd8efa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a0e8047f5d0ae39f0d",
+    "url": "/INFO655/react-playlist/static/js/3.7fa99968.chunk.js"
+  },
+  {
+    "revision": "1beb52bfedd10a66c36b",
+    "url": "/INFO655/react-playlist/static/js/main.60795281.chunk.js"
+  },
+  {
+    "revision": "b8900b51a225f30bc82f",
+    "url": "/INFO655/react-playlist/static/js/runtime-main.49cef27f.js"
+  },
+  {
+    "revision": "152cd78f80d7018454b2fc82a0fc9795",
+    "url": "/INFO655/react-playlist/static/media/GreenGuitarLogo.152cd78f.png"
+  }
+]);
